5683ebcf16af93ed14d3637a3a0235d28e1637a1
fumihiro.sunada <fumihiro.sunada@bigtreetc.com>
2024-03-02 21:49:08 +0900

    Add S3 Copy Sample Code.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # s3-copy-with-mm
 Amazon S3 file copy with multi-thread &amp; multipart sample.
+
+# Amazon S3 cross region copy performance
+
+以下方式のS3クロスリージョン転送時間（東京→大阪）を計測しました。
+
+* boto3.client('s3').copy_object()
+* boto3.resource('s3').meta.client.copy()
+* boto3.client('s3').upload_part_copy() <- This Sample Code
+
+その結果、本サンプルが最も速くデータを転送できました。（下図）
+
+![alt text](assets/image.png)
+
+# Total Result
+
+データ転送だけでなくタグやメタデータのCopyも含めた方式の比較結果は下表のとおりです。
+
+| 方式 | タグ | メタデータ | 速度 | 上限サイズ | 備考 |
+| :--- | :---: | :---: | :---: | :---: | :--- |
+| boto3.client('s3').copy_object() | O<br />Copyされる | O<br />Copyされる | X<br />遅い | X<br />5GB | 最も低速だがタグやメタデータが一緒にCopyされる |
+| boto3.resource('s3').meta.client.copy() | X<br />Copyされない | X<br />Copyされない | O<br />速い | O<br />5TB | 高速だがタグやメタデータが基本的にCopyされない |
+| boto3.client('s3').upload_part_copy() | O<br />Copyされる | O<br />Copyされる | O<br />速い | O<br />5TB | 最も高速でタグやメタデータもCopyできる |
+
+以上の結果から、ファイルサイズが大きく、タグやメタデータも含め高速にCopyする場合は、本サンプルの方式が最も優れていました。
+
+# Appendix.
+
+## How to make big test data file for Windows
+
+```
+fsutil file createnew bigfile_001mb 1048576
+fsutil file createnew bigfile_010mb 10485760
+fsutil file createnew bigfile_100mb 104857600
+fsutil file createnew bigfile_500mb 524288000
+fsutil file createnew bigfile_01gb 1073741824
+fsutil file createnew bigfile_02gb 2147483648
+fsutil file createnew bigfile_05gb 5368709120
+fsutil file createnew bigfile_08gb 8589934592
+fsutil file createnew bigfile_10gb 10737418240
+fsutil file createnew bigfile_20gb 21474836480
+```
---
+++ lambda_function.py
@@ -0,0 +1,152 @@
+import json
+import boto3
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+
+s3c = boto3.client("s3")
+s3r = boto3.resource('s3')
+
+def print_log(*arg: tuple):
+    '''ログ出力
+    '''
+    print(*arg)
+
+def upload_part_copy(src_bucket, src_key, target_bucket, target_key, part_num, copy_source_range, upload_id, parts_etags):
+    '''マルチパートでS3オブジェクトの一部（指定範囲）をコピーする
+    '''
+    try:
+        response = s3c.upload_part_copy(
+            Bucket=target_bucket,
+            CopySource={'Bucket': src_bucket, 'Key': src_key},
+            CopySourceRange=copy_source_range,
+            Key=target_key,
+            PartNumber=part_num,
+            UploadId=upload_id
+        )
+        parts_etags.append({"ETag": response["CopyPartResult"]["ETag"], "PartNumber": part_num})
+    except Exception as e:
+        print_log(f"Error while CREATING UPLOAD_PART_COPY")
+        raise e
+
+def copy_with_multipart(src_bucket, src_key, target_bucket, target_key, object_size, metadata):
+    '''マルチパートでS3オブジェクトをコピーする
+    '''
+    try:
+        initiate_multipart = s3c.create_multipart_upload(
+            Bucket=target_bucket,
+            Key=target_key,
+            Metadata=metadata
+        )
+        upload_id = initiate_multipart['UploadId']
+        #print_log("upload_id: ", upload_id)
+        part_size = 64 * 1024 * 1024 # part size[byte]
+        #print_log("part_size: ", part_size)
+        byte_position = 0
+
+        parts_etags = []
+
+        max_range = -(-object_size // part_size) # 切り上げ
+        #print_log("max_range: ", max_range)
+
+        with ThreadPoolExecutor(max_workers=10) as executor: # s3.ap-northeast-3.amazonaws.com. Connection pool size: 10
+            for part_num in range(1, max_range + 1):
+                last_byte = min(byte_position + part_size - 1, object_size - 1)
+                copy_source_range = f"bytes={byte_position}-{last_byte}"
+                executor.submit(upload_part_copy, src_bucket, src_key, target_bucket, target_key, part_num, copy_source_range, upload_id, parts_etags)
+                byte_position += part_size
+                sleep(1)
+        try:
+            parts_etags = sorted(parts_etags, key=lambda x:x['PartNumber'])
+            response = s3c.complete_multipart_upload(
+                Bucket=target_bucket,
+                Key=target_key,
+                MultipartUpload={
+                    'Parts': parts_etags
+                },
+                UploadId=upload_id
+            )
+            print_log(f"COMPLETE_MULTIPART_UPLOAD COMPLETED SUCCESSFULLY, response={response}")
+        except Exception as e:
+            print_log(f"Error while CREATING COMPLETE_MULTIPART_UPLOAD")
+            raise e
+    except Exception as e:
+        print_log(f"Error while CREATING CREATE_MULTIPART_UPLOAD")
+        raise e
+
+
+def lambda_handler(event, context):
+
+    print_log('start')
+
+    # S3バケット名
+    src_bucket_name = 'sun-ada-bucket-tokyo'
+    tgt_bucket_name = 'sun-ada-bucket-osaka'
+
+    # S3オブジェクト名
+    cpy_object_name = 'bigfile_001mb'
+    #cpy_object_name = 'bigfile_010mb'
+    #cpy_object_name = 'bigfile_100mb'
+    #cpy_object_name = 'bigfile_500mb'
+    #cpy_object_name = 'bigfile_01gb'
+    #cpy_object_name = 'bigfile_02gb'
+    #cpy_object_name = 'bigfile_05gb'
+    #cpy_object_name = 'bigfile_08gb'
+    #cpy_object_name = 'bigfile_10gb'
+    #cpy_object_name = 'bigfile_20gb'
+
+    copy_source = {
+        'Bucket': src_bucket_name,
+        'Key': cpy_object_name
+    }
+
+    print_log(cpy_object_name)
+    
+    try:
+        # コピー元オブジェクト情報
+        print_log(src_bucket_name)
+        src_res_head_object = s3c.head_object(Bucket=src_bucket_name, Key=cpy_object_name)
+        src_metadata = src_res_head_object["Metadata"]
+        print_log("Metadata : ", src_metadata)
+
+        src_res_get_object_tagging = s3c.get_object_tagging(Bucket=src_bucket_name, Key=cpy_object_name)
+        src_tagset = src_res_get_object_tagging["TagSet"]
+        print_log("TagSet : ", src_tagset)
+
+        contentLength = src_res_head_object["ContentLength"]
+        print_log("ContentLength : : ", contentLength)
+
+        # オブジェクトコピー
+        #s3c.copy_object(CopySource=copy_source, Bucket=tgt_bucket_name, Key=cpy_object_name)
+        #s3r.meta.client.copy(copy_source, tgt_bucket_name, cpy_object_name)
+        copy_with_multipart(
+            src_bucket_name,
+            cpy_object_name,
+            tgt_bucket_name,
+            cpy_object_name,
+            contentLength,
+            src_metadata
+        )
+        # タグ情報もコピー先へ設定
+        s3c.put_object_tagging(Bucket=tgt_bucket_name, Key=cpy_object_name, Tagging={'TagSet': src_tagset})
+
+        # コピー先オブジェクト情報
+        print_log(tgt_bucket_name)
+        tgt_res_head_object = s3c.head_object(Bucket=tgt_bucket_name, Key=cpy_object_name)
+        tgt_metadata = tgt_res_head_object["Metadata"]
+        print_log("Metadata : ", tgt_metadata)
+
+        tgt_res_get_object_tagging = s3c.get_object_tagging(Bucket=tgt_bucket_name, Key=cpy_object_name)
+        tgt_tagset = src_res_get_object_tagging["TagSet"]
+        print_log("TagSet : ", tgt_tagset)
+
+        print_log('success')
+        
+    except Exception as e:
+        print_log('error: ', e)
+
+    finally:
+        print_log('finally')
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
